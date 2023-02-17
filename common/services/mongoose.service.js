475856ec3b515273342fe2d28259bc087d84faaa
BadKernel <106186643+DevBadKernel@users.noreply.github.com>
2023-02-17 20:40:20 +0100

    Update mongoose.service.js

--- common/services/mongoose.service.js
+++ common/services/mongoose.service.js
@@ -8,16 +8,19 @@ const options = {
     useUnifiedTopology:true
 }
 
+// admin de mongodb "actiuhumaMongodb" psw "1234567890" db "rlp"
+// usuario "actiuhuma" psw "rlpdatabase"  db"rlp"
+
 const connectWithRetry = ()=>{
     console.log('Mongo connection with retry');
     mongoose.set('strictQuery',true);
-    mongoose.connect('mongodb://actiuhumaMongodb1:rlpdatabase@127.0.0.1:27017/rlp',options)
+    mongoose.connect('mongodb://actiuhuma:rlpdatabase@127.0.0.1:27017/rlp',options)
     .then(()=>{
-        console.log('Mongo is connected');
+        console.log('Mongo is connected 🟢');
 
     }).catch(error=>{
         console.log('Mongo connection unsuccessful, retry after 5 seconds. ', ++count);
-        console.log('Mongo connection error: ', error);
+        console.log('Mongo connection error 🔴: ', error);
         setTimeout(connectWithRetry, 5000);
     })
 }
