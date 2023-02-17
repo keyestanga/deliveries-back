0c70d2e4c84d5f9f1087795ec7716dabbc023610
jquerolmar <jquerolmar@uoc.edu>
2023-02-17 20:05:15 +0100

    backend MEAN Antonio

---
+++ users/routes/user.routes.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const usersController = require('../controllers/user.controller');
+
+
+exports.userRoutes = function(app){
+    
+    app.post('/api/login-user',usersController,loginUser);
+    
+    app.post('/api/register-user',usersController,registerUser);
+
+    app.put('/api/upload/user-pic',usersController,uploadUserPic);
+
+}
